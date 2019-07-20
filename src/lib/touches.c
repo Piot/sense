c7bf2fef3735f674abb59f4b3d3368924b07d95b
Peter Bjorklund <piot@hotmail.com>
2019-07-20 09:30:43 +0200

    chore: compile warning

--- src/lib/touches.c
+++ src/lib/touches.c
@@ -53,7 +53,7 @@ const char* sense_touch_phase_name(sense_touch_phase phase)
 	return phase_to_string(phase);
 }
 
-void sense_touch_debug(const sense_touch* self)
+static void sense_touch_debug(const sense_touch* self)
 {
 	CLOG_INFO("[touch id:%zu x:%d y:%d %s]", self->identifier, self->position.x, self->position.y, phase_to_string(self->phase));
 }
