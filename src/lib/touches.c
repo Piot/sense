221b730c73b5d39dfe284a92df07bf9d269d6c02
Peter Bjorklund <piot@hotmail.com>
2023-08-08 16:32:10 +0200

    fix: compile warning

--- src/lib/touches.c
+++ src/lib/touches.c
@@ -38,14 +38,14 @@ const char* sense_touch_phase_name(sense_touch_phase phase)
 static void sense_touch_debug(const sense_touch* self)
 {
 	(void) self;
-	CLOG_INFO("[touch id:%zu x:%d y:%d %s]", self->identifier, self->position.x, self->position.y, phase_to_string(self->phase));
+	CLOG_INFO("[touch id:%zu x:%d y:%d %s]", self->identifier, self->position.x, self->position.y, phase_to_string(self->phase))
 }
 #endif
 
 void sense_touches_add(sense_touches* self, size_t identifier, sense_touch_phase phase, BlVector2i position)
 {
 	if (self->touch_count >= self->max_touch_count) {
-		CLOG_OUTPUT("Out of touch space! %zu of %zu", self->touch_count, self->max_touch_count);
+		CLOG_OUTPUT("Out of touch space! %zu of %zu", self->touch_count, self->max_touch_count)
 		return;
 	}
 	//latch_mutex_lock(&self->mutex);
